6221df87b5c39169f7e9ba934689490626f14322
Jack Gao <jackgao1540@gmail.com>
2022-02-11 14:39:49 -0800

    Updated README.md with a more concise description of Phase 1, and added a bit of UI.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 ## A video game based on one of my favourite animes
 
 - What will this application do?
-  - The final stage of this application will be a cute video game where chibi titans are attacking your town and you have to defeat them.
+  - The final stage of this application will be a cute video game where chibi titans are attacking your town, and you have to defeat them.
 - Who will use this app?
   - People who want to have fun playing a cute game based on a famous anime.
 - Why is this project of interest to me?
-  - Because I really like AOT and video games.
+  - Because I really like AOT and video games. This first phase will simply implement a system for modelling the environment. The graphics and physics of the game have already been deeply brainstormed by me but it is a little too early to implement them fully as of right this first stage.
 
 ## User Stories
 - As a user, I want to be able to add an item to my inventory.
--- src/main/ui/AttackOnTitanApp.java
+++ src/main/ui/AttackOnTitanApp.java
@@ -3,6 +3,7 @@ package ui;
 import model.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 // AttackOnTitanApp
@@ -10,13 +11,57 @@ public class AttackOnTitanApp {
     private Player player;
     private ArrayList<Item> shopItems;
     private ArrayList<Titan> titans;
+    private Scanner input;
+
+    public static final Integer MAX_CHARS = 26;
+    public static final Integer MIN_CHARS = 2;
 
     public AttackOnTitanApp() {
         init();
     }
 
     public void init() {
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
         //prompt user for their name
+        System.out.println("Please enter your name: ");
+        String s = input.next();
+        while (!validName(s)) {
+            System.out.println("Please enter a valid name (no spaces, length between " + MIN_CHARS.toString() + " and " + MAX_CHARS.toString() + ").");
+            s = input.next();
+        }
+        player = new Player(s);
 
+        //initialize the titans
+        titans = new ArrayList<>(Arrays.asList(
+                new Titan("Connie"),
+                new Titan("Sasha"),
+                new Titan("Eren"),
+                new Titan("Mikasa"),
+                new Titan("Armin"),
+                new Titan("Reiner"),
+                new Titan("Bertholdt"),
+                new Titan("Annie"),
+                new Titan("Jeke")));
+
+        //initialize the shop
+        shopItems = new ArrayList<>(Arrays.asList(
+                new Item("Rusty Kitchen Knife", 1),
+                new Item("Meat Pie for Sasha", 3),
+                new Item("Zeke's Baseball", 4),
+                new Item("Sword", 10),
+                new Item("ODM Gear", 50),
+                new Item("Thunder Spear",  100),
+                new Item("Founding Titan Fluid", 999)));
+    }
+
+    boolean validName(String s) {
+        if (s.length() >= MAX_CHARS || s.length() <= MIN_CHARS) {
+            return false;
+        }
+        if (s.contains(" ")) {
+            return false;
+        }
+        return true;
     }
 }
